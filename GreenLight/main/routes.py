47a79400111681efd54649d59734cdd51ea8554a
NTIvanova21 <NTIvanova21@codingburgas.bg>
2025-05-17 16:46:56 +0300

    Create the base for the admin page. Close #7

--- GreenLight/main/routes.py
+++ GreenLight/main/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, redirect, flash
-from flask_login import login_user, logout_user, current_user
+from flask_login import login_user, logout_user, current_user, login_required
 from .. import db
 
 from . import main_bp
@@ -11,3 +11,9 @@ from .. import login_manager
 @main_bp.route('/home')
 def home():
     return render_template("homePage.html")
+
+@main_bp.route('/show_accounts', methods=['GET', 'POST'])
+@login_required
+def show_accounts():
+    all_accounts = User.query.all()
+    return render_template("adminPage.html", accounts=all_accounts)
